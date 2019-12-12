264e95b3cfc9610e535cad9f3e5ec077cceec4ae
JYn0 <j_y__n@naver.com>
2019-12-13 02:01:50 +0900

    ready to deploy go

--- ootd/settings.py
+++ ootd/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'sx5we^$%&5v5xbi6*5=@y%wrxfqfkmk6a9e33ei^rl#5j65#_#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '']
 
